ddd341d7f624308e42e44513162084809f846a47
J. Cohen <hostelmarketing@gmail.com>
2013-10-31 11:19:12 -0700

    Preparing for habitat

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Chatroom
 
 This is an experiment with MongoDB. It's designed to be mostly client-side. The backend is Express because it's easy to deploy.
+
+## Extras
+
+Needs an .env file as mentioned here: http://stackoverflow.com/questions/18565512/important-of-session-secret-key-in-express-web-framework
+
+The DB connection stuff is in /db which is not in version control.
--- app.js
+++ app.js
@@ -8,7 +8,11 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var env = (function(){
+    var Habitat = require("habitat");
+    Habitat.load();
+    return new Habitat();
+}());
 var app = express();
 
 // all environments
@@ -21,17 +25,25 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.cookieParser());
-app.use(express.session({secret: '937rufsgrEWirjtt8GR38877s652FHb'}));
+// app.use(express.session({secret: 'keyboard cat'}));
+app.use(express.cookieSession({
+    key: "mysite.sid",
+    secret: env.get("SESSION_SECRET"),
+    cookie: {
+        maxAge: 2678400000 // 31 days
+    }
+}));
+app.use(express.csrf());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
